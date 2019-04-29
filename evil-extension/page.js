e08794c6376a96204e3f7b5ae28ef40070f6a85f
ejk2013421 <ejk2013421@gmail.com>
2019-04-29 16:02:48 +0800

    part1 and part2

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -12,9 +12,42 @@ const MATCH_LIST = {
 
 function transformTextNodes(node) {
   // TODO(you): Implement this function! See HW spec for details.
+  if (node.nodeType === Node.TEXT_NODE) {
+    console.log('text:' + node.textContent);
+    //node.replace(index,item);
+    /*for(var key in MATCH_LIST){
+      node.textContent = node.textContent.replace(/there/g,MATCH_LIST[key]);
+    }*/
+    node.textContent = node.textContent.replace(/\bthere\b/g,"$$$the_match_1@@@");
+    node.textContent = node.textContent.replace(/\btheir\b/g,"$$$the_match_2@@@");
+    node.textContent = node.textContent.replace(/\bthey're\b/g,"$$$the_match_3@@@");
+    node.textContent = node.textContent.replace(/\bThere\b/g,"$$$the_match_4@@@");
+    node.textContent = node.textContent.replace(/\bTheir\b/g,"$$$the_match_5@@@");
+    node.textContent = node.textContent.replace(/\bThey're\b/g,"$$$the_match_6@@@");
+    node.textContent = node.textContent.replace(/\bTHERE\b/g,"$$$the_match_7@@@");
+    node.textContent = node.textContent.replace(/\bTHEIR\b/g,"$$$the_match_8@@@");
+    node.textContent = node.textContent.replace(/\bTHEY'RE\b/g,"$$$the_match_9@@@");
+
+    node.textContent = node.textContent.replace(/\$\$the_match_1\@\@\@/g,MATCH_LIST['there']);
+    node.textContent = node.textContent.replace(/\$\$the_match_2\@\@\@/g,MATCH_LIST['their']);
+    node.textContent = node.textContent.replace(/\$\$the_match_3\@\@\@/g,MATCH_LIST["they're"]);
+    node.textContent = node.textContent.replace(/\$\$the_match_4\@\@\@/g,MATCH_LIST['There']);
+    node.textContent = node.textContent.replace(/\$\$the_match_5\@\@\@/g,MATCH_LIST['Their']);
+    node.textContent = node.textContent.replace(/\$\$the_match_6\@\@\@/g,MATCH_LIST["They're"]);
+    node.textContent = node.textContent.replace(/\$\$the_match_7\@\@\@/g,MATCH_LIST['THERE']);
+    node.textContent = node.textContent.replace(/\$\$the_match_8\@\@\@/g,MATCH_LIST['THEIR']);
+    node.textContent = node.textContent.replace(/\$\$the_match_9\@\@\@/g,MATCH_LIST["THEY'RE"]);
+
+  } else {
+    //console.log(level + root.nodeName);
+  }
+  for (const child of node.childNodes) {
+    transformTextNodes(child);
+  }
 }
 
 transformTextNodes(document.body);
 
 // Log statement to test that the extension loaded properly.
 console.log('Evil extension loaded!');
+console.log('Extension updated');
--- script.js
+++ script.js
@@ -2,3 +2,79 @@
 
 // You can access the RESULTS_MAP from "constants.js" in this file since
 // "constants.js" has been included before "script.js" in index.html.
+
+function complete_check() {
+    const len=document.querySelectorAll('.chosen').length;
+
+    if(len==3){
+        const boxes = document.querySelectorAll('.choice-grid div');
+        for (const box of boxes) {
+            box.removeEventListener('click', change_checked);
+        }
+        Allitem= document.querySelectorAll('div.chosen');
+        const item1= Allitem[0].dataset.choiceId;
+        const item2= Allitem[1].dataset.choiceId;
+        const item3= Allitem[2].dataset.choiceId;
+        console.log(item1);
+        console.log(item2);
+        console.log(item3);
+
+        document.getElementById('result_section').hidden=false;
+
+        if(item1 == item2){
+            document.getElementById('result_title').innerHTML=RESULTS_MAP[item1]['title'];
+            document.getElementById('result_content').innerHTML=RESULTS_MAP[item1]['contents'];
+        }
+        else if(item1 == item3){
+            document.getElementById('result_title').innerHTML=RESULTS_MAP[item1]['title'];
+            document.getElementById('result_content').innerHTML=RESULTS_MAP[item1]['contents'];
+        }
+        else if(item2 == item3){
+            document.getElementById('result_title').innerHTML=RESULTS_MAP[item2]['title'];
+            document.getElementById('result_content').innerHTML=RESULTS_MAP[item2]['contents'];
+        }
+        else{
+            document.getElementById('result_title').innerHTML=RESULTS_MAP[item1]['title'];
+            document.getElementById('result_content').innerHTML=RESULTS_MAP[item1]['contents'];
+        }
+    }
+
+}
+
+function change_checked(event) {
+    console.log(event.currentTarget.dataset.questionId);
+    const items = document.querySelectorAll('.choice-grid div[data-question-id='+event.currentTarget.dataset.questionId+']');
+    for (const item of items) {
+        item.querySelector("img + img").src="images/unchecked.png";
+        item.classList='';
+        item.classList.add('unchosen');
+    }
+    console.log(event.currentTarget.dataset.choiceId);
+    dst=document.querySelector('div[data-question-id='+event.currentTarget.dataset.questionId+' ][data-choice-id='+event.currentTarget.dataset.choiceId+']');
+    dst.querySelector("img + img").src="images/checked.png";
+    dst.classList='chosen';
+
+    complete_check();
+}
+
+// ----
+// MAIN
+// ----
+
+const boxes = document.querySelectorAll('.choice-grid div');
+for (const box of boxes) {
+    box.addEventListener('click', change_checked);
+}
+
+function reset() {
+    const boxes = document.querySelectorAll('.choice-grid div');
+    for (const box of boxes) {
+        box.addEventListener('click', change_checked);
+        box.classList="";
+        img = box.querySelectorAll("img + img");
+        for (const i of img){
+            i.src="images/unchecked.png";
+        }
+    }
+    document.getElementById('result_section').hidden=true;
+}
